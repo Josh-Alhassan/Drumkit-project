eb47e8977bcdfc3e6fd61ba5724acf450e6623c7
Alhassan Joshua Abel <41088053+Saint-Josh@users.noreply.github.com>
2022-03-14 03:38:39 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 function playSound(e) {
-	const audio = document.querySelector(`audio[data-key = "${e.keycode}"]`);
-	const key = document.querySelector(`key[data-key = "${e.keycode}"`);
+	const audio = document.querySelector(`audio[data-key = "${e.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key = "${e.keycode}"]`);
 
 	if (!audio) return; // Stop the function from running
 	audio.currentTime = 0; // Rewind Audio to start
 	audio.play();
-	key.classlist.add('playing');
+	key.classList.add('playing');
 }
 
 function removeTransition(e) {
-	if (e.propertyName !== 'transform') {
-		return // Skip if it is not a transfrom
-	}
-	this.classlist.remove('playing');
+	if (e.propertyName !== 'transform') return; // Skip if it is not a transfrom
+	this.classList.remove('playing');
 }
 
 
@@ -20,3 +18,5 @@ const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitioned', removeTransition));
 
 window.addEventListener('keydown', playSound);
+
+console.log('HI')
